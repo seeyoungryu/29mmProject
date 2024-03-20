39c38f4bbec43362cd663bfa5b8bbdd9b3020982
seeyoungryu <seeyoungryu@gmail.com>
2024-03-20 17:10:33 +0900

    Initial commit

---
+++ src/test/java/com/example/_29mm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._29mm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
